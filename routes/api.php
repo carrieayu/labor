a2091b6554aea7c4885a9d096fe419f9d6d99ed6
Carre Yu <carrie@bpoc.co.jp>
2022-08-05 15:00:38 +0800

    updates

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\TimeCardController;
+use App\Http\Controllers\IcController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +20,7 @@ use App\Http\Controllers\TimeCardController;
 Route::get('/getAllUser', [UserController::class, 'index'])->name('userInfos.user');
 Route::get('/getUserById/{id}', [UserController::class, 'show']);
 Route::get('/getTimeCard/{id}', [TimeCardController::class, 'show']);
+Route::get('/getIC', [IcController::class, 'index']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();

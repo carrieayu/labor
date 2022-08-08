b862bd65acc4c33db498cea46c6da01b28980b9d
PuhJan <ferjohn@bpoc.co.jp>
2022-08-08 15:20:27 +0800

    Added Company Controller and Group Controller

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\TimeCardController;
+<<<<<<< Updated upstream
 use App\Http\Controllers\IcController;
 
+=======
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\GroupController;
+>>>>>>> Stashed changes
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -20,8 +25,14 @@ use App\Http\Controllers\IcController;
 Route::get('/getAllUser', [UserController::class, 'index'])->name('userInfos.user');
 Route::get('/getUserById/{id}', [UserController::class, 'show']);
 Route::get('/getTimeCard/{id}', [TimeCardController::class, 'show']);
+<<<<<<< Updated upstream
 Route::get('/getIC', [IcController::class, 'index']);
 Route::get('/getAllTC', [TimeCardController::class, 'index']);
+=======
+Route::get('/getAllTc', [TimeCardController::Class,'index']);
+Route::get('/getAllCompany', [CompanyController::class, 'index']);
+Route::get('getAllGroups', [GroupController::class, 'index']);
+>>>>>>> Stashed changes
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
